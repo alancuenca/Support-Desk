f215a5af5d3f7a9f5cafe97a47ca8853ef5fd652
Alan Cuenca <alan.r.cuenca@gmail.com>
2023-01-30 13:46:25 -0800

    create authMiddleware security

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -69,6 +69,13 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 });
 
+// @desc get current user
+//@route /api/users/me
+//@access Private
+const getMe = asyncHandler(async (req, res) => {
+    res.send('Me')
+});
+
 // Generate Token
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -78,5 +85,6 @@ const generateToken = (id) => {
 
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getMe
 };
