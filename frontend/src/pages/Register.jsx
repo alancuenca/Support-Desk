d761066ead9e0d9f5d64bc365c2b643de244c58d
Alan Cuenca <alan.r.cuenca@gmail.com>
2023-01-31 12:17:35 -0800

    bring in toast

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import {toast} from 'react-toastify'
 import { FaUser } from "react-icons/fa"
 
 function Register() {
@@ -11,7 +12,14 @@ function Register() {
 
     const { name, email, password, password2 } = formData
 
-    
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+
+        }))
+    }
+
     return (
         <>
             <section className="heading">
@@ -21,16 +29,53 @@ function Register() {
                 <p>Please create an account</p>
             </section>
             <section className="form">
-                <form>
+                <form onSubmit={onSubmit}>
                     <div className="form-group">
                         <input
                             type="text"
                             className="form-control"
                             id="name"
+                            name="name"
                             value={name}
                             onChange={onChange}
                             placeholder="Enter your name"
                         />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="email"
+                            name="email"
+                            value={email}
+                            onChange={onChange}
+                            placeholder="Enter your email"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            name="password"
+                            value={password}
+                            onChange={onChange}
+                            placeholder="Create a password"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password2"
+                            className="form-control"
+                            id="password2"
+                            name="password2"
+                            value={password2}
+                            onChange={onChange}
+                            placeholder="Re-enter password"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-block">Submit</button>
                     </div>
                 </form>
             </section>
