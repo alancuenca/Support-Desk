755d77b9795178fd7ecd51a46f5ca37c2bf34779
Alan Cuenca <alan.r.cuenca@gmail.com>
2023-02-02 11:54:42 -0800

    connect ticket routes backend

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -104,12 +104,42 @@ const deleteTicket = asyncHandler(async (req, res) => {
     // this is new
     await ticket.remove()
 
-    res.status(200).json({success: true});
+    res.status(200).json({ success: true });
+});
+
+// @desc edit single ticket
+//@route PUT /api/tickets/:id
+//@access Private
+const editTicket = asyncHandler(async (req, res) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('user not found')
+    }
+    const ticket = await Ticket.findById(req.params.id)
+
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    //limit to user
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+
+    const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).json(updatedTicket);
 });
 
 module.exports = {
     getTickets,
     createTicket,
     getTicket,
-    deleteTicket
+    deleteTicket,
+    editTicket
 }
