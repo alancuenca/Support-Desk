d735f801a4badf2d39a252e0319670619173c8ae
Alan Cuenca <alan.r.cuenca@gmail.com>
2023-02-02 14:30:21 -0800

    tweeks to ticket slice and service

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -2,21 +2,20 @@ import axios from 'axios'
 
 const API_URL = '/api/tickets/'
 
-// Create new ticket
+//Create new ticket
 const createTicket = async (ticketData, token) => {
     const config = {
         headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    const response = await axios.post(API_URL, ticketData, config);
 
-    const response = await axios.post(API_URL, ticketData, config)
-
-    return response.data
-}
+    return response.data;
+};
 
 const ticketService = {
     createTicket
-}
+};
 
 export default ticketService;
