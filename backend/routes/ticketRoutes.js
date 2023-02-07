f936b097dedb24bfe81b139165a5001ece2a343f
Alan Cuenca <alan.r.cuenca@gmail.com>
2023-02-06 21:59:30 -0800

    init noteRoutes

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -4,6 +4,10 @@ const { getTickets, createTicket, getTicket, deleteTicket, editTicket } = requir
 
 const { protect } = require('../middleware/authMiddleware')
 
+// re-route into note router
+const noteRouter = require('./noteRoutes')
+router.use('/:ticketId/notes', noteRouter)
+
 router.route('/').get(protect, getTickets).post(protect, createTicket)
 
 router.route('/:id').get(protect, getTicket).delete(protect, deleteTicket).put(protect, editTicket)
